97348e7a1184bd228560cb357a970e57c71f7a20
Manuel Iglesias <6154160+manueliglesias@users.noreply.github.com>
2023-08-07 12:34:31 -0700

    wip

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "webdsplusapi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { default as awsConfig } from "./aws-exports";
+import { Amplify, DataStore, DataStoreRuntimeContext, ModelBuilder } from "aws-amplify";
+import { listTodos } from "./graphql/queries";
+import { createTodo } from "./graphql/mutations";
+
+Amplify.Logger.LOG_LEVEL = "DEBUG";
+
+Amplify.configure(awsConfig);
+
+const { Todo } = DataStoreRuntimeContext.create({
+  Todo: ModelBuilder.instance()
+    .field("id", "ID")
+    .field("name", "String")
+    .field("name2", 'AWSDate')
+    .field("cosa", 'Boolean')
+    .syncQuery("listTodos", listTodos)
+    .mutations({
+      INSERT: { fieldName: 'createTodoOld', document: createTodo}
+    })
+});
+
+const todo = new Todo({
+  name: '',
+  cosa: false,
+  name2: ''
+});
+
+Todo.copyOf(todo, d => {
+  d.name = '';
+  
+  // @ts-expect-error
+  d.id = 'asdasdasd';
+
+  // @ts-expect-error
+  d.name2 = false;
+  
+  // @ts-expect-error
+  d.cosa = 'true';
+  
+  d.name = '';
+  d.name2 = 'asdasdasd';
+  d.cosa = true;
+});
 
 function App() {
+  useEffect(() => {
+    DataStore.start();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +66,19 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={onQuery}>QUERY</button>
+        <button onClick={onAdd}>ADD</button>
       </header>
     </div>
   );
+}
+
+async function onQuery() {
+
+}
+
+async function onAdd() {
+
 }
 
 export default App;
